c919b6f9059368f6a26f60c10ed7403274e775de
llmind <yongkang.xie@gmail.com>
2026-03-30 17:15:37 +0800

    merge changes to test0002.py

--- LeetCode/Atom/test0002.py
+++ LeetCode/Atom/test0002.py
@@ -1,6 +1,7 @@
-import os
+﻿import os
 import sys
 
 print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+print("你好世界")
